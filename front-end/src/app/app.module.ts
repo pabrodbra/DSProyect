60dfdfe476b5667fbc91b878b169b7f878d61990
Oliver López <olivervilod46@gmail.com>
2019-03-12 23:24:00 +0100

    Added components+routes

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,21 +6,32 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 //Componentes
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { TvshowsComponent } from './components/tvshows/tvshows.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/log/login/login.component';
+import { SignupComponent } from './components/log/signup/signup.component';
 
 //Rutas
 import { APP_ROUTING } from './app.routes';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
+    MoviesComponent,
+    TvshowsComponent,
+    ProfileComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
-    APP_ROUTING
+    APP_ROUTING,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,8 +1,18 @@
-import { Routes, RouterModule } from '@angular/router'
-import {HomeComponent} from './components/home/home.component'
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { TvshowsComponent } from './components/tvshows/tvshows.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/log/login/login.component';
+import { SignupComponent } from './components/log/signup/signup.component';
 
 const app_routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'tvshows', component: TvshowsComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
