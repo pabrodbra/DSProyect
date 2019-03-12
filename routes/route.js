d29ade7c4355599407456ad8d00e142ab813bebd
Pablo <pabrod@uma.es>
2019-03-12 04:23:53 +0100

    Minor fix

--- routes/route.js
+++ routes/route.js
@@ -5,36 +5,43 @@ const router = express.Router();
 const Movie = require('../models/movies');
 
 router.get('/movies', (req, res)=>{
-    //res.send('Retrieving contact list');
-    Movie.find((err, contacts)=>{
-        res.json(contacts);
+    //res.send('Retrieving movie list');
+    Movie.find((err, movies)=>{
+        res.json(movies);
     })
 });
 
-// Add contact
-router.post('/contact', (req, res, next)=>{
+// Add movie
+router.post('/movie', (req, res, next)=>{
     let newMovie = new Movie({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        phone: req.body.phone
+        title: req.body.title,
+        year: req.body.year,
+        genres: req.body.genres,
+        ratings: req.body.ratings,
+        duration: req.body.duration,
+        releaseDate: req.body.releaseDate,
+        storyline: req.body.storyline,
+        actors: req.body.actors,
+        imdbRating: req.body.imdbRating,
+        posterurl: req.body.posterurl
     });
 
-    newMovie.save((err, contact)=>{
+    newMovie.save((err, movie)=>{
         if(err) res.json({msg: 'Failed to add movie'});
         else res.json({msg: 'Movie added succesfully'});
     });
 });
 
-// Delete contact
-router.delete('/contact/:id', (req, res, next)=>{
+// Delete movie
+router.delete('/movie/:id', (req, res, next)=>{
     Movie.remove({_id: req.params.id}, (err, result)=>{
         if(err) res.json(err);
         else res.json(result);
     })
 });
 
-// Update contact
-router.post('/contact', (req, res, next)=>{
+// Update movie
+router.post('/movie', (req, res, next)=>{
 
 });
 
